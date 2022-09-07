cc2b45056105445a208cffbb0242ed72c7987512
AlexSash1 <80047578+AlexSash1@users.noreply.github.com>
2022-09-07 15:30:03 +0300

    Update Update_Status_of_reply.ps1

--- Update_Status_of_reply.ps1
+++ Update_Status_of_reply.ps1
@@ -1,3 +1,9 @@
+# Applies to: Exchange Server 2016 Enterprise Edition, Exchange Server 2016 Standard Edition, Exchange Server 2013 Standard Edition, Exchange Server 2013 Enterprise, Exchange Server 2010 Enterprise, Exchange Server 2010 Standard, Exchange Online
+# When the Out of Office Assistant is enabled, only one reply is sent to each sender, even if you receive multiple messages from that person
+# The Out of Office Assistant sends an automatic reply to notify users who send you messages that you are away from the office. Your reply is only sent once to a message sender. 
+# The count is reset when you toggle the Out of Office Assistant. Microsoft Exchange clears its internal "sent to" list when you disable the Out of Office Assistant.
+#
+# This script toggles the Out of Office Assistant in mailboes where AutoReplyState is enabled
 $mailboxes = Get-Mailbox | Get-MailboxAutoReplyConfiguration | Where-Object {$_.AutoReplyState –eq “enabled”} 
 
 foreach ($mailbox in $mailboxes) {
